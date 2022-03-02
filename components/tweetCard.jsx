2957d6609a6fecebcbce6ebd13fed9a949d2bb24
dsereboo <dsereboo@gmail.com>
2022-03-02 12:01:02 +0000

    Twitter intent feature implemented

--- components/tweetCard.jsx
+++ components/tweetCard.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { IconContext } from "react-icons";
 import { FaTwitter } from "react-icons/fa";
 import styles from "../styles/tweetCard.module.css" 
+import { Share } from "react-twitter-widgets";
 
 const TweetCard = ({ data }) => {
   return (
@@ -11,12 +12,16 @@ const TweetCard = ({ data }) => {
           <FaTwitter />
         </IconContext.Provider>
         <p>{data}</p>
-        <Link  href="https://twitter.com/intent/tweet?text=Hello%20world">
+        <div className="mt-2">
+            <Share options={{size:"large", text:`${data}`}}/>
+        </div>
+      
+        {/* <Link  href={"https://twitter.com/intent/tweet?text="+`${data}`}>
         <button className={"button is-success is-normal mt-2 "+`${styles.twitterButton}`}>
            <FaTwitter />
           <p className="ml-2 has-text-weight-bold">Tweet</p>
         </button>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
